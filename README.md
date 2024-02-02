ef57dc1528da4954da3d77da6e035ce159d82f36
Anish Singla <singlaanish56@gmail.com>
2024-02-03 05:01:55 +0530

    Static site

---
+++ README.md
@@ -0,0 +1,2 @@
+quick site taken from trunarla's (thank you so much)
+https://gist.github.com/tnarla/0c09a11fea366145ba684fe6ebf578c5
---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+    const [noCount, setNoCount] = useState(0)
+    const [yesCount, setYesCount] = useState(0)
+    const [yesPressed, setYesPressed] = useState(false)
+    const [hr, setHR] = useState(false)
+    const yesButtonSize = noCount *10  + 15
+    const noButtonSize = yesCount *10  + 15
+
+    const handleYesClick = () => {
+      setYesCount(yesCount+1)
+    }
+
+    const handleNoClick = () => {
+      setNoCount(noCount+1)
+    }
+
+    const getNoButtonText = () => {
+      if(hr) setHR(false)
+      const phrases= [
+        "No",
+        "No ?????",
+        "Will you break my heart like that ?",
+        "You might regret this",
+        "Maybe I deserve this",
+        "Cries ensues",
+        "For Hello Kitty in Exchange ?",
+        "Try Again !",
+        "I have a small heart too.",
+        "For all the cats out there",
+        "Pretty pleaseeeeeeeeee",
+        "Afsana bana ke bhool mat jaana",
+        "Teri yaad saath hai",
+        "Ik vari haan karde mutiyare"
+      ]
+
+      if(noCount==11 || noCount==12) setHR(true)
+      if(noCount==phrases.length) setNoCount(0);
+
+      return phrases[Math.min(noCount, phrases.length -1)]
+    }
+
+    const getYesButtonText = () => {
+      if(hr) setHR(false)
+      const phrases= [
+        "Yes",
+        "Are you sure about that ?",
+        "Phas jaoge",
+        "Ironically Himesh Reshamiya sunauga",
+        "I talk too much",
+        "Still time to click no",
+        "Please click for confirmation",
+        "thanks !!!"
+      ]
+      if(yesCount==3) setHR(true)
+      if(yesCount==phrases.length -1) setYesPressed(true)
+      return phrases[Math.min(yesCount, phrases.length -1)]
+    }
+
+  return (
+    <>
+      <div className='flex flex-col mx-auto items-center h-screen justify-center'>
+        { yesPressed ? 
+        (<>
+          <img src="https://gifdb.com/images/high/milk-bear-love-mocha-cute-hug-o4qy1xq5p6myps41.gif" alt="" />
+          <h1 className='text-4xl text-center font-sans mx-auto text-red-500'>Yayyyyyyyyyyyyyyyyyyyyyyyyyyy !!!!</h1>
+        </>)
+        :
+        (<>
+            <img className='h-[200px]' src="https://gifdb.com/images/high/bear-love-bouncing-mocha-with-flowers-n4m0fyne6h4e6y2x.gif" alt="" />
+            <h1 className='text-4xl text-center font-sans mx-auto my-10'>Will you be my Valentine !</h1>
+            <div className='flex flex-row items-center justify-between space-x-5 mx-auto mt-10'>
+                <button 
+                onClick={handleYesClick}
+                className=' text-white bg-green-500 px-10 py-6 mx-auto rounded-md hover:bg-green-700' 
+                style={{fontSize: yesButtonSize}}>
+                {yesCount==0 ? "Yes" : getYesButtonText()}</button>
+                <button 
+                onClick={handleNoClick}
+                className='text-white bg-red-500  px-10 py-6 mx-auto rounded-md hover:bg-red-700'
+                style={{fontSize: noButtonSize}}>
+                {noCount==0 ? "No" : getNoButtonText()}</button>
+            </div>
+        </>)}
+      </div>
+    </>
+  )
+}
+
+export default App
